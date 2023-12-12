b11f07487adb9cf42f3cfe609173efa864adc366
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2023-12-13 02:48:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,280 @@
+
+
+// if1
+// let n = +prompt('n sonini kiriting');
+// let res;
+
+// if(n > 0){
+//     res = n + 1;
+// }else{
+//     res = n;
+// }
+// console.log(res);
+
+
+
+
+//f2
+// let n = +prompt("n sonini kiriting");
+// let res;
+// if(n >= 0){
+//     if(n == 0){
+//         res = 10;
+//     }else{
+//         res = n + 1;
+//     }
+// }else{
+//     res = n - 2; 
+// }
+
+// console.log(res);
+
+
+
+
+//f3
+// let a = +prompt('a sonini kiriting');
+// let b = +prompt('b sonini kiriting');
+// let c = +prompt('c sonini kiriting');
+
+// let PositiveCount = 0;
+// let NegativeCount = 0;
+
+// if (a > 0) {
+//     PositiveCount++;
+// } else if(a < 0) {
+//     NegativeCount++;
+// }
+// if (b > 0) {
+//     PositiveCount++;
+// } else if (b < 0) {
+//     NegativeCount++;
+// }
+// if (c > 0) {
+//     PositiveCount++;
+// }else if(c < 0) {
+//     NegativeCount++;
+// }
+
+// console.log(PositiveCount , "musbat sonlar soni");
+// console.log(NegativeCount , "manfiy sonlar soni");
+
+
+
+
+
+//f4
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let res;
+
+// if(a > b){
+//     res = a;
+// }else if(b > a){
+//     res = b;
+// }
+
+// console.log(res);
+
+
+
+
+//f5
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let res;
+
+// if(a > b){
+//     res = 2;
+// }else if(b > a){
+//     res = 1;
+// }
+
+// console.log(res , "kichik son tartib raqami");
+
+
+
+//f6
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+
+// if(a > b){
+//      console.log(a);
+//      console.log(b);
+// }else if(b > a){
+//      console.log(b);
+//      console.log(a);
+// }
+
+
+
+
+//f7
+// let a = +prompt('a sonni kiriting');
+// let b = +prompt('b sonni kiriting');
+
+// let res = a;
+// if(a > b){
+//     a = b;
+//     b = res;
+//     console.log(b , ">" , a);
+// }else if(b > a){
+//     console.log(b , ">" , a);
+// }
+
+
+
+
+
+//f8
+// let a = +prompt('a sonni kiriting');
+// let b = +prompt('b sonni kiriting');
+
+// let res = a;
+// if(a != b){
+//     a = a + b;
+//     b = res + b;
+// }else if(b = a){
+//     a = 0;
+//     b = 0;
+// }
+// console.log(a , "a ning qiymati");
+// console.log(b , "b ning qiymati");
+
+
+
+//f9 
+// let a = +prompt('a sonini kiriting');
+// let b = +prompt('b sonini kiriting');
+// let res = a;
+// if(a != b){
+//     if(a > b){
+//         a;
+//         b = a;
+//     }
+//     else{
+//         b;
+//         a = b;
+//     }
+// }else if(b = a){
+//     a = 0;
+//     b = 0;
+// }
+// console.log(a , "a ning qiymati");
+// console.log(b , "b ning qiymati");
+
+
+
+//f10
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// let res;
+
+// if(a > b){
+//     if(b > c){
+//         res = c;
+//     }else{
+//         res = b;
+//     }
+// }else if(a < b){
+//     if(a > c){
+//         res = c;
+//     }else{
+//         res = a;
+//     }
+// }
+
+// console.log(res);
+
+
+
+
+
+//11
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+
+// let res;
+
+// if((a > b , b > c) || (a < b , b < c)){
+//     res = b;
+// }else if((b > a , a > c) || (b < a , a < c)){
+//     res = a;
+// }else if(((a > c , c > b) || (a < c , c < b))){
+//     res = c;
+// }
+
+// console.log(res);
+
+
+
+
+
+
+//f12
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+
+// let res1;
+// let res2;
+
+// if(a > b > c){
+//     res1 = a;
+//     res2 = b;
+// }else{
+//     res1 = b;
+//     res2 = c;
+// }
+
+// if(b > a > c){
+//     res1 = b;
+//     res2 = a;
+// }else{
+//     res1 = a;
+//     res2 = c;
+// }
+
+// if(a > c > b){
+//     res1 = a;
+//     res2 = c;
+// }else{
+//     res1 = c;
+//     res2 = b;
+// }
+
+// console.log(res1);
+// console.log(res2);
+
+
+
+
+//f13
+
+
+
+
+
+
+
+
+
+
+
+
+
+//f2
+// let a = +prompt('a sonni kiriting');
+// let b = +prompt('b sonni kiriting');
+// let c = +prompt('c sonni kiriting');
+
+// let number = 0;
+// let res;
+
+// if(a > 0 , b > 0 , c > 0){
+//     res = number + 3;
+// }
+
+// console.log(res);
