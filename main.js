4b0e2ede8f0dd172c949ffe057253cbfc5d10842
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2023-12-20 21:56:12 +0500

    fill the task

--- main.js
+++ main.js
@@ -251,10 +251,46 @@
 
 
 
+// if1
+// let n = +prompt('n sonini kiriting');
+// let res;
+
+// if(n > 0){
+//     res = n + 1;
+// }else{
+//     res = n;
+// }
+// console.log(res);
+
+
+
 //f13
 
 
 
+// let A = +prompt("A sonini kiriting");
+// let B = +prompt("B sonini kiriting");
+// let C = +prompt("C sonini kiriting");
+
+// let res1;
+// let res2;
+// let res3;
+
+// if(A < B && B < C){
+//      res1 = 2 * A ;
+//      res2 = 2 * B ;
+//      res3 = 2 * C ;
+
+// }else{
+//     res1 = -A ;
+//     res2 = -B ;
+//     res3 = -C ;
+
+// }
+ 
+// console.log(res1);
+// console.log(res2);
+// console.log(res3);
 
 
 
@@ -262,19 +298,353 @@
 
 
 
+//f14
+// let A = +prompt("A sonini kiriting");
+// let B = +prompt("B sonini kiriting");
+// let C = +prompt("C sonini kiriting");
+
+// if((A < B && B < C) || (A > B && B > C)){
+//     res1 = 2 * A ;
+//     res2 = 2 * B ;
+//     res3 = 2 * C ;
+// }else{
+//     res1 = -A ;
+//     res2 = -B ;
+//     res3 = -C ;
+// }
+
+// console.log(res1);
+// console.log(res2);
+// console.log(res3);
 
 
 
-//f2
-// let a = +prompt('a sonni kiriting');
-// let b = +prompt('b sonni kiriting');
-// let c = +prompt('c sonni kiriting');
+//f15
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// let d = +prompt("d sonini kiriting");
 
-// let number = 0;
+// if(a ==b && b == c){
+//     res1 = "d son oxirida 4 - joylashgan"
+// }else if(a == c && c == d){
+//     res1 = "b soni 2-da joylashgan"
+// }else if(b == c && c == d){
+//     res1 = "a soni 1-da joylashgan"
+// }else{
+//     res1 = "c soni 3-da joylashgan"
+// }
+
+// console.log(res1);
+
+
+//f16
+
+// let x = +prompt("x sonini kiriting")
+
+
 // let res;
-
-// if(a > 0 , b > 0 , c > 0){
-//     res = number + 3;
+// if(x <= 0){
+//     res = -x;
+// }else if( 0 < x && x < 2){
+//     res = x ** 2;
+// }else{
+//     res = 4;
 // }
 
 // console.log(res);
+
+
+
+//f17
+// let y = +prompt( 'y yilni kiriting')
+
+// if(y % 4 == 0){
+//     y = y / 400;
+//     res = y * 365;
+// }else{
+//     y = 
+// }
+
+
+//f18
+// let n = +prompt('n sonini kiriting');
+
+// if(n / 10 < 1 && n % 2 ==0){
+//     res = "bir xonali juft son"
+// }else if(n / 10 < 1 && n % 2 == 1){
+//     res = "bir xonali toq son"
+// }else if(n / 10 < 10 && n % 2 == 0){
+//     res = "ikki xonali juft son"
+// }else if(n / 10 < 10 && n % 2 == 1){
+//     res = "ikki xonali toq son"
+// }else if(n / 10 < 100 && n % 2 == 0){
+//     res = "uch xonali juft son"
+// }else if(n/10 < 100 && n % 2 == 1){
+//     res = "uch xonali toq son"
+// }
+
+// console.log(res)
+
+
+
+//Case1
+// let k = +prompt('k sonini kiriting');
+// switch(k){
+//     case 1:
+//     console.log("yomon");
+//     break
+//     case 2:
+//     console.log("qoniqarsiz");
+//     break
+//     case 3:
+//     console.log("qoniqarli");
+//     break
+//     case 4:
+//     console.log("yaxshi");
+//     break
+//     case 5:
+//     console.log("alo");
+//     break
+// }
+
+
+
+//Case2
+// let oy = +prompt("oyni kiriting")
+// switch(oy){
+//     case 1:
+//     console.log("qish");
+//     break
+//     case 2:
+//     console.log("qish");
+//     break
+//     case 3:
+//     console.log("qish");
+//     break
+//     case 4:
+//     console.log("bahor");
+//     break
+//     case 5:
+//     console.log("bahor");
+//     break
+//     case 6:
+//     console.log("bahor");
+//     break
+//     case 7:
+//     console.log("yoz");
+//     break
+//     case 8:
+//     console.log("yoz");
+//     break
+//     case 9:
+//     console.log("yoz");
+//     break
+//     case 10:
+//     console.log("kuz");
+//     break
+//     case 11:
+//     console.log("kuz");
+//     break
+//     case 12:
+//     console.log("kuz");
+//     break
+// }
+
+
+
+//Case3
+// let oy = +prompt('oy ni kiriting')
+// switch(oy){
+//     case 1:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 2:
+//     console.log("bu oyda 28 kun bor");
+//     break
+//     case 3:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 4:
+//     console.log("bu oyda 30 kun bor");
+//     break
+//     case 5:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 6:
+//     console.log("bu oyda 30 kun bor");
+//     break
+//     case 7:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 8:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 9:
+//     console.log("bu oyda 31 kun bor");
+//     break
+//     case 10:
+//     console.log("bu oyda 30 kun bor");
+//     break
+//     case 11:
+//     console.log("bu oyda 30 kun bor");
+//     break
+//     case 12:
+//     console.log("bu oyda 31 kun bor");
+//     break
+// }
+
+
+
+//Case 4
+// let n = +prompt('tartib raqam kiriting')
+// let l = +prompt(' uzunlikni kiriting');
+// switch(l , n){
+//     case 1:
+//     console.log(l /10 , "metr");
+//     break
+//     case 2:
+//     console.log(l * 1000 , "metr");
+//     break
+//     case 3:
+//     console.log(l , "metr");
+//     break
+//     case 4:
+//     console.log(l / 1000 , "metr");
+//     break
+//     case 5:
+//     console.log(l /100 , "metr");
+//     break
+// }
+
+
+
+
+//Case 5
+// let n = +prompt('tartib raqam kiriting')
+// let kg = +prompt(' og`irlikni kiriting');
+// switch(l , n){
+//     case 1:
+//     console.log(kg , "kilogramm");
+//     break
+//     case 2:
+//     console.log(kg / 10000 , "kilogramm");
+//     break
+//     case 3:
+//     console.log(kg / 1000 , "kilogramm");
+//     break
+//     case 4:
+//     console.log(kg * 1000 , "kilogramm");
+//     break
+//     case 5:
+//     console.log(kg * 100 , "kilogramm");
+//     break
+// }
+
+
+
+
+//Case6
+// let d = +prompt('kun ni kiriting');
+// let m = +prompt(' oy ni kiriting');
+
+// switch(m){
+//     case 1:
+//     console.log(d , "- yanvar");
+//     break
+//     case 2:
+//     console.log(d , "- fevral");
+//     break
+//     case 3:
+//     console.log(d , "- mart");
+//     break
+//     case 4:
+//     console.log(d , "- aprel");
+//     break
+//     case 5:
+//     console.log(d , "- may");
+//     break
+//     case 6:
+//     console.log(d , "- iyun");
+//     break
+//     case 7:
+//     console.log(d , "- iyul");
+//     break
+//     case 8:
+//     console.log(d , "- avgust");
+//     break
+//     case 9:
+//     console.log(d , "- sentabr");
+//     break
+//     case 10:
+//     console.log(d , "- oktabr");
+//     break
+//     case 11:
+//     console.log(d , "- noyabr");
+//     break
+//     case 12:
+//     console.log(d , "- dekabr");
+//     break
+// }
+
+
+//Case7
+// let d = +prompt('kun ni kiriting');
+// let m = +prompt(' oy ni kiriting');
+
+// switch(m){
+//     case 1:
+//     console.log(d + 1 , "- yanvar");
+//     break
+//     case 2:
+//     console.log(d + 1 , "- fevral");
+//     break
+//     case 3:
+//     console.log(d + 1 , "- mart");
+//     break
+//     case 4:
+//     console.log(d + 1 , "- aprel");
+//     break
+//     case 5:
+//     console.log(d + 1 , "- may");
+//     break
+//     case 6:
+//     console.log(d + 1 , "- iyun");
+//     break
+//     case 7:
+//     console.log(d + 1 , "- iyul");
+//     break
+//     case 8:
+//     console.log(d + 1 , "- avgust");
+//     break
+//     case 9:
+//     console.log(d + 1 , "- sentabr");
+//     break
+//     case 10:
+//     console.log(d + 1 , "- oktabr");
+//     break
+//     case 11:
+//     console.log(d + 1 , "- noyabr");
+//     break
+//     case 12:
+//     console.log(d + 1 , "- dekabr");
+//     break
+// }
+
+
+
+
+//Case8
+// let  k = +prompt('command kirit');
+
+// switch(k){
+//     case 0:
+//     console.log("shimol");
+//     break
+//     case 1:
+//     console.log("g'arb");
+//     break
+//     case 0:
+//     console.log("sharq");
+//         break
+// }
